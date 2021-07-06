26700883ef6dbcc5ad5b06430980995102191e14
Jeff Akins <jeffakins21@gmail.com>
2021-07-06 12:53:30 -0700

    Problems 1 and 2 complete for the temp tables exercises

--- temporary_tables.sql
+++ temporary_tables.sql
@@ -7,7 +7,8 @@ CREATE TEMPORARY TABLE germain_1469.employees_with_departments AS
 SELECT first_name, last_name, dept_name
 FROM employees
 JOIN dept_emp ON dept_emp.emp_no = employees.emp_no
-JOIN departments ON departments.dept_no = dept_emp.dept_no;
+JOIN departments ON departments.dept_no = dept_emp.dept_no; 
+-- Created temp table with name employees_with_departments
 
 SELECT * 
 FROM germain_1469.employees_with_departments;
@@ -15,7 +16,7 @@ FROM germain_1469.employees_with_departments;
 -- a. Add a column named full_name to this table. It should be a VARCHAR whose length is the sum of the lengths of the first name and last name columns
 
 ALTER TABLE germain_1469.employees_with_departments
-	ADD full_name VARCHAR(100);
+	ADD full_name VARCHAR(100); -- Add column
 	
 SELECT * 
 FROM germain_1469.employees_with_departments;	
@@ -23,7 +24,7 @@ FROM germain_1469.employees_with_departments;
 -- b. Update the table so that full name column contains the correct data
 
 UPDATE germain_1469.employees_with_departments 
-	SET full_name = CONCAT(first_name, " ", last_name); 
+	SET full_name = CONCAT(first_name, " ", last_name); -- Add data to new column
 
 SELECT * 
 FROM germain_1469.employees_with_departments;	
@@ -31,10 +32,10 @@ FROM germain_1469.employees_with_departments;
 -- c. Remove the first_name and last_name columns from the table.
 
 ALTER TABLE germain_1469.employees_with_departments
-	DROP COLUMN first_name;
+	DROP COLUMN first_name; -- Remove first_name column
 
 ALTER TABLE germain_1469.employees_with_departments
-	DROP COLUMN last_name;
+	DROP COLUMN last_name; -- Remove last_name column
 
 SELECT * 
 FROM germain_1469.employees_with_departments;	
@@ -45,10 +46,35 @@ SELECT dept_name, full_name
 FROM germain_1469.employees_with_departments;	
 
 
+
 -- 2. Create a temporary table based on the payment table from the sakila database. Write the SQL necessary to transform the amount column such that it is stored as an integer representing the number of cents of the payment. For example, 1.99 should become 199.
 
+CREATE TEMPORARY TABLE germain_1469.sakila_payment AS
 SELECT * 
-FROM payment;
+FROM payment; -- Creating the temp table with name sakila_payment
+
+SELECT *
+FROM germain_1469.sakila_payment; -- Check to see if the info is in the table
+
+DESCRIBE germain_1469.sakila_payment; -- To see the 'type' for the amount column
+
+ALTER TABLE germain_1469.sakila_payment
+	MODIFY COLUMN amount DECIMAL(10,2); -- modify the type of the amount column so that I can perform scalar multiplication on the column
+	
+UPDATE germain_1469.sakila_payment
+	SET amount = amount * 100; -- multiply the column by 100
+	
+ALTER TABLE germain_1469.sakila_payment
+	MODIFY COLUMN amount INT; -- change the column type to integer
+
+SELECT *
+FROM germain_1469.sakila_payment; -- check to see that the information in the table has been modified/updated
+
+DROP TABLE germain_1469.sakila_payment; -- If needed
+
+
+
+-- 3. Find out how the current average pay in each department compares to the overall, historical average pay. In order to make the comparison easier, you should use the Z-score for salaries. In terms of salary, what is the best department right now to work for? The worst?
 
 
 
